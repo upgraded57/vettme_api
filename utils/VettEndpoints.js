687a40d6a196bce6ef5c6ccc9c10edbad96850e3
upgraded57 <shokunbiupgraded@gmail.com>
2024-09-04 11:00:27 +0100

    Updated phone number endpoint

--- utils/VettEndpoints.js
+++ utils/VettEndpoints.js
@@ -1,7 +1,7 @@
 const endpoints = {
   bvn: "api/v1/kyc/bvn/advance",
   nin: "api/v1/kyc/nin",
-  phone_number: "api/v1/kyc/phone_number/basic",
+  phone_number: "api/v1/kyc/phone_number",
   nuban: "api/v1/kyc/nuban",
   drivers_licence: "api/v1/kyc/dl",
   voters_id: "api/v1/kyc/vin",
